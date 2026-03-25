95690f03c1bd13806b508c0d5e59a7ace4da8ce5
rafa <rafaondjango@gmail.com>
2026-03-25 18:33:58 -0300

    feat: Implement trigram generation and frequency counting from a text dataset.

--- token_presumer.py
+++ token_presumer.py
@@ -1,2 +1,31 @@
 from collections import defaultdict, Counter
-from typing import List, Dict, Tuple
+from typing import List, Tuple
+
+ngram_counts = defaultdict(Counter)
+
+with open("dataset.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+paragraphs = text.split("\n\n")
+
+def space_tokenizer(paragraph: str) -> List[str]:
+    return paragraph.split()
+
+tokens = [token for paragraph in paragraphs for token in space_tokenizer(paragraph)]
+
+def generate_ngrams(tokens: List[str], n: int) -> List[Tuple[str, ...]]:
+    return [tuple(tokens[i:i+n]) for i in range(len(tokens) - n + 1)]
+
+ngrams = generate_ngrams(tokens, 3)
+
+for ngram in ngrams:
+    context = ngram[:-1]
+    token = ngram[-1]
+    ngram_counts[context][token] += 1
+
+ngram_counts_result = {
+    " ".join(context): dict(counter)
+    for context, counter in ngram_counts.items()
+}
+
+print(ngram_counts_result)
